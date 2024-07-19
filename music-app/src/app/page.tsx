36e481b69b09c17cafa68a36e79e09b4080e63d4
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-07-19 16:06:24 +0530

    video 5 completed successfully but taiwind config need to correct

--- music-app/src/app/page.tsx
+++ music-app/src/app/page.tsx
@@ -1,7 +1,14 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
 export default function Home (){
   return (
     <main className="min-h-screen bg-black/[0.95] antialiased bg-grid-white text-white">
-      <h1 className="text-center">Chai aur code </h1> </main> 
+      <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      </main> 
   )
 } 
 
